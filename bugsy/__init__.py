d63e55129032279847aa9281f79bbacbb24a3872
pyoor <pyoor@users.noreply.github.com>
2019-09-19 16:41:16 -0400

    Export exceptions directly from errors

--- bugsy/__init__.py
+++ bugsy/__init__.py
@@ -1,4 +1,5 @@
 from .attachment import Attachment  # noqa
 from .bug import Bug, BugException, Comment  # noqa
-from .bugsy import Bugsy, BugsyException, LoginException  # noqa
+from .bugsy import Bugsy  # noqa
+from .errors import *  # noqa
 from .search import Search  # noqa
